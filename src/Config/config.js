8889c4ae27a06b6fa0458f4d0dfcdd726251ac60
Pranshu mishra <Pranshu0777@gmail.com>
2019-07-17 17:08:57 +0530

    initial commit

---
+++ src/Config/config.js
@@ -0,0 +1,11 @@
+
+  export const DB_CONFIG = {
+    apiKey: "AIzaSyCwxu301NpnOHoQ-NfGvafEL62dZhHVFQA",
+    authDomain: "todo-5d257.firebaseapp.com",
+    databaseURL: "https://todo-5d257.firebaseio.com",
+    projectId: "todo-5d257",
+    storageBucket: "",
+    messagingSenderId: "761825484314",
+    appId: "1:761825484314:web:aa481909538656e2"
+  };
+  
