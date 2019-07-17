e75c7381588cedc29f05cfa5e1b3911599f470ca
Pranshu mishra <Pranshu0777@gmail.com>
2019-07-17 17:19:43 +0530

    Updates

---
+++ precache-manifest.033cea34ea639f5c1c1ad05dce00960b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ece90650273cccec0d7a7aa08a70498",
+    "url": "/PranshuMishra07/todo.git/index.html"
+  },
+  {
+    "revision": "f19869e3781dfe5bb1ce",
+    "url": "/PranshuMishra07/todo.git/static/css/main.3ffacde9.chunk.css"
+  },
+  {
+    "revision": "234a74f67bc4e529386b",
+    "url": "/PranshuMishra07/todo.git/static/js/2.d619ee41.chunk.js"
+  },
+  {
+    "revision": "f19869e3781dfe5bb1ce",
+    "url": "/PranshuMishra07/todo.git/static/js/main.942a38e2.chunk.js"
+  },
+  {
+    "revision": "c1afa6fa717c800c72b6",
+    "url": "/PranshuMishra07/todo.git/static/js/runtime~main.6681d45f.js"
+  },
+  {
+    "revision": "03f7c8db0291dd7ab3cea2626aed7da1",
+    "url": "/PranshuMishra07/todo.git/static/media/attractive-beautiful-beauty-839347.03f7c8db.jpg"
+  }
+]);
